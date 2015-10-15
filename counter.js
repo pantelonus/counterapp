daa5c0f686184c1872f1afe39d303e5efda1acb3
Andrew Maitland <pantelonus@gmail.com>
2015-10-15 14:16:00 -0500

    Restructures file path, updates JavaScript

--- counter.js
+++ counter.js
@@ -1,40 +1,49 @@
 
-var output = 0;
+var output = 0,
+    updateOutput = document.getElementById('output'),
+    resetBalance = document.getElementById('reset'),
+    menuButton = document.getElementById('menu_button'),
+    menu = document.getElementById('menu'),
+    toggle;
 
-function resetBalance() {
+resetBalance.addEventListener('click', function () {
 	var reset = confirm('Do you want to reset your balance to 0.00?');
 	if (reset) {
 		output = 0;
-		var update = document.getElementById('output');
-		update.innerHTML = output.toFixed(2);
+		updateOutput.innerHTML = output.toFixed(2);
 	}
-}
+});
+
+
+document.body.addEventListener('click', function(e){
+  e = e || window.event;
+  var target = e.target, x;
+  if (target.className.match('minus')) {
+    x = parseFloat(target.parentNode.id);
+    minus(x);
+  } else if (target.className.match('plus')) {
+    x = parseFloat(target.parentNode.id);
+    plus(x);
+  }
+});
 
 function plus(amt) {
 	output += amt;
-	var update = document.getElementById('output');
-	update.innerHTML = output.toFixed(2);
+	updateOutput.innerHTML = output.toFixed(2);
 }
 
 function minus(amt) {
 	output -= amt;
-	var update = document.getElementById('output');
-	update.innerHTML = output.toFixed(2);
+	updateOutput.innerHTML = output.toFixed(2);
 }
 
-var toggle;
-
-function menuSlide() {
-	var menuButton = document.getElementById("menu_button");
-	var menu = document.getElementById("menu");
-	
-	if(toggle) {
-		menuButton.style.left = "1em";
-		menu.style.left = "-53%";
-		toggle = false;
-	} else {
-		menuButton.style.left = "8.5em";
-		menu.style.left = "0";
-		toggle = true;
-	}
-}
+menuButton.addEventListener('click', function () {
+  toggle = toggle ?
+		(menuButton.style.left = "1em",
+		menu.style.left = "-53%",
+		toggle = false)
+	:
+		(menuButton.style.left = "8.5em",
+		menu.style.left = "0",
+		toggle = true);
+});

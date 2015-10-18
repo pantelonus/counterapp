a436c6b3f10915521f44e133e08236c43746fafc
Andrew Maitland <pantelonus@gmail.com>
2015-10-18 13:15:31 -0500

    Completes overlay element coding to be responsive based on selection (menu/reset); adjusts 'reset' element to absolute position (for z-index placement)

--- counter.js
+++ counter.js
@@ -7,14 +7,68 @@ var output = 0,
     overlay = document.getElementById('overlay'),
     menu = {
       dom: document.getElementById('menu'),
-      toggled: false
+      toggled: false,
+      transition: function() {
+        this.toggled = this.toggled ?
+		    (menuButton.style.left = "1em",
+		    menu.dom.style.left = "-53%",
+		    this.toggled = false)
+	    :
+		    (menuButton.style.left = "8.5em",
+		    menu.dom.style.left = "0",
+		    this.toggled = true);
+		    overlayTransition();
+        }
     },
-    resetBalance = {
+    reset = {
       dom: document.getElementById('reset'),
-      toggled: false
-    },
-    toggle;
+      toggled: false,
+      transition: function() {
+        this.toggled = this.toggled ?
+  	  	(resetConfirm.style.top = "-25%",
+  	  	resetCancel.style.top = "-25%",
+  	  	this.toggled = false)
+  	  :
+  	    (resetConfirm.style.top = "3.5em",
+  	  	resetCancel.style.top = "7em",
+  	  	this.toggled = true);
+  	  	overlayTransition();
+       }
+    };
 
+// Functions:
+
+function plus(amt) {
+	output += amt;
+	updateOutput.innerHTML = output.toFixed(2);
+}
+
+function minus(amt) {
+	output -= amt;
+	updateOutput.innerHTML = output.toFixed(2);
+}
+
+function overlayTransition() {
+  if (menu.toggled === true || reset.toggled === true) {
+    overlay.style.opacity = "0.75";
+    overlay.style.zIndex = "2";
+    if (reset.toggled) {
+      menuButton.style.zIndex = "1";
+    }
+    if (menu.toggled) {
+      reset.dom.style.zIndex = "1";
+    }
+  } else {
+    overlay.style.opacity = "0";
+    setTimeout(function(){
+      overlay.style.zIndex = "-1";
+      menuButton.style.zIndex = "4";
+      reset.dom.style.zIndex = "4";
+    }, 500); //allots for opacity transition time
+  }
+}
+
+// Add event listeners:
 
 document.body.addEventListener('click', function(e){
   e = e || window.event;
@@ -27,46 +81,25 @@ document.body.addEventListener('click', function(e){
     plus(x);
   }
 });
-
-function plus(amt) {
-	output += amt;
-	updateOutput.innerHTML = output.toFixed(2);
-}
-
-function minus(amt) {
-	output -= amt;
-	updateOutput.innerHTML = output.toFixed(2);
-}
-
-function reset() {
-  toggle = toggle ?
-  		(resetConfirm.style.top = "-10%",
-  		resetCancel.style.top = "-10%",
-  		overlay.style.opacity = "0",
-  		toggle = false)
-  	:
-  		(resetConfirm.style.top = "3.5em",
-  		resetCancel.style.top = "7em",
-  		overlay.style.opacity = "0.75",
-  		toggle = true);
-}
-
-menuButton.addEventListener('click', function () {
-  toggle = toggle ?
-		(menuButton.style.left = "1em",
-		menu.dom.style.left = "-53%",
-		toggle = false)
-	:
-		(menuButton.style.left = "8.5em",
-		menu.dom.style.left = "0",
-		toggle = true);
+menuButton.addEventListener('click', function() {
+  menu.transition();
 });
-
-resetBalance.dom.addEventListener('click', reset);
+reset.dom.addEventListener('click', function(){
+  reset.transition();
+});
 resetConfirm.addEventListener('click', function(){
   output = 0;
   updateOutput.innerHTML = output.toFixed(2);
-  reset();
+  reset.transition();
 });
-resetCancel.addEventListener('click', reset);
-overlay.addEventListener('click', reset);
+resetCancel.addEventListener('click', function(){
+  reset.transition();
+});
+overlay.addEventListener('click', function(){
+  overlayTransition();
+  if (reset.toggled) {
+  reset.transition();
+  } else if (menu.toggled) {
+    menu.transition();
+  }
+});
